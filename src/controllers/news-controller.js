bb3e6b48958b8fe3415cd95b5c2efadc2aa7b003
qb-anandureghu <anandu.reghu@qburst.com>
2023-05-03 17:15:52 +0530

    resolved prams finding issue while getting news

--- src/controllers/news-controller.js
+++ src/controllers/news-controller.js
@@ -1,11 +1,12 @@
 const newsService = require("../services/news-service");
+const userService = require("../services/user-service");
 
 const GetAllNews = async (req, res) => {
   const user = req.user;
   const params = {
     lang: "eng"
   }
-  params.keyword = user.preferences || []
+  params.keyword = userService.getUserPreferences(user.userId) || []
   params.articlesCount = req.params.count ? req.params.count : 10;
 
   newsService.getAllNews(params).then((result) => {
